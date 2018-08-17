e1b1461843347ed51a63e383d6c500d79b3f3970
Patrick Shobe <patrick@patrickshobe.com>
2018-08-17 10:54:56 -0600

    Patrick Shobe

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,18 @@
 class LaughTracksApp < Sinatra::Base
+
+  get '/comedians' do
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+      erb :show
+    else
+      @comedians = Comedian.all
+      @average_age = Comedian.average(:age).round(2)
+      erb :index
+    end
+  end
+
+  get '/comedians/:age' do
+    @comedians = Comedian.where(age: params[:age])
+    erb :show
+  end
 end
--- app/models/special.rb
+++ app/models/special.rb
@@ -1,3 +1,7 @@
 class Special < ActiveRecord::Base
   validates :name, presence: true
+
+  def self.comedian_specials(comedian_id)
+    self.where(comedian_id: comedian_id)
+  end
 end
