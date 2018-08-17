b562a0135f34f8ae4aee1161fc5ef8ac55265486
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-08-17 11:03:06 -0600

    initial commit

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,4 +1,16 @@
 class Comedian < ActiveRecord::Base
+  has_many :specials
   validates :name, presence: true
   validates :age, presence: true
+
+  def self.comedians_average_age
+   average(:age)
+  end
+
+  def self.total_tv_specials
+   sum(:specials)
+  end
+
 end
+
+
