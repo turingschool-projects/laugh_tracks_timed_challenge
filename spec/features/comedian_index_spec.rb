0ffa85def85dc3d8f194ff5ea867e145e0f38233
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-17 10:36:44 -0600

    ATP for index showing comedian name and age

--- spec/features/comedian_index_spec.rb
+++ spec/features/comedian_index_spec.rb
@@ -1,8 +1,19 @@
 RSpec.describe 'Comedian index page' do
+  before(:each) do
+    iliza = Comedian.create(name: "Iliza Shlesinger", age: 34)
+    ali = Comedian.create(name: "Ali Wong", age: 35)
+  end
 
-  it 'should show merchant name' do
-    visit "/merchants/new"
-    expect(page).to have_content('Create New Merchant')
+  it 'should show comedian name' do
+    visit "/comedians"
+    expect(page).to have_content('Iliza')
+    expect(page).to_not have_content('Hannibal')
+  end
+
+  it 'should show comedian age' do
+    visit "/comedians"
+    expect(page).to have_content('34')
+    expect(page).to_not have_content('36')
   end
 
 end
