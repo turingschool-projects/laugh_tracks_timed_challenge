29b5ceb370c12142d5926016ecf11da44a12144b
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-17 10:26:37 -0600

    Adds Specials to index view

--- app/models/special.rb
+++ app/models/special.rb
@@ -1,3 +1,4 @@
 class Special < ActiveRecord::Base
+  belongs_to :comedian
   validates :name, presence: true
 end
--- spec/features/comedians_index_spec.rb
+++ spec/features/comedians_index_spec.rb
@@ -3,11 +3,14 @@ RSpec.describe 'Comedians Index Page' do
     it 'should show all comdians' do
       comedian_1 = Comedian.create(name: "George Lopez", age: 56)
       comedian_2 = Comedian.create(name: "Ian Bagg",age: 43)
+      special_1 = Special.create(name: "Laughs", comedian_id: 1)
+      special_2 = Special.create(name: "Funny", comedian_id: 2)
 
       visit '/comedians'
 
       expect(page).to have_content("George Lopez")
       expect(page).to have_content("Age: 43")
+      expect(page).to have_content("Funny")
     end
   end
 end
