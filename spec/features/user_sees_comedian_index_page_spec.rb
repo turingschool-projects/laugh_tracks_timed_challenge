aaf3a486b1da887fcdb2239c6295cce887931c31
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-17 10:42:49 -0600

    tests and methods for user story 2

--- spec/features/user_sees_comedian_index_page_spec.rb
+++ spec/features/user_sees_comedian_index_page_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe 'Comedian index page' do
   before(:each) do
     @comic_1 = Comedian.create(name: 'Mitch Hedberg', age: 48)
+    @special_1 = @comic_1.specials.create(name: 'Christmas Special')
     @comic_2 = Comedian.create(name: 'Mitchy Hedberg', age: 42)
   end
 
@@ -11,11 +12,18 @@ RSpec.describe 'Comedian index page' do
       expect(page).to have_content(@comic_1.name)
       expect(page).to have_content(@comic_2.name)
     end
-    it 'should show comedian ages' do
+    it 'should show all comedian ages' do
       visit '/comedians'
 
       expect(page).to have_content(@comic_1.age)
       expect(page).to have_content(@comic_2.age)
     end
   end
-end 
+  context 'shows comedian TV specials' do
+    it 'should show all comedian TV specials' do
+      visit '/comedians'
+
+    expect(page).to have_content(@comic_1.specials.name)
+    end
+  end
+end
