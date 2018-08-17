f2504c5de8e7ca5e89ad24c200c37645a3f4f097
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-17 10:48:42 -0600

    Adds search by age in uri

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -2,4 +2,8 @@ class Comedian < ActiveRecord::Base
   has_many :specials
   validates :name, presence: true
   validates :age, presence: true
+
+  def self.average_age
+    average(:age)
+  end
 end
--- spec/features/comedians_index_spec.rb
+++ spec/features/comedians_index_spec.rb
@@ -1,16 +1,25 @@
 RSpec.describe 'Comedians Index Page' do
   context 'a vistor to the page' do
-    it 'should show all comdians' do
-      comedian_1 = Comedian.create(name: "George Lopez", age: 56)
-      comedian_2 = Comedian.create(name: "Ian Bagg",age: 43)
-      special_1 = Special.create(name: "Laughs", comedian_id: 1)
-      special_2 = Special.create(name: "Funny", comedian_id: 2)
+    before(:each) do
+      @comedian_1 = Comedian.create(name: "George Lopez", age: 56)
+      @comedian_2 = Comedian.create(name: "Ian Bagg",age: 44)
+      @special_1 = Special.create(name: "Laughs", comedian_id: 1)
+      @special_2 = Special.create(name: "Funny", comedian_id: 2)
+
+    end
+    it 'should show all comedians names, ages, specials' do
 
       visit '/comedians'
 
       expect(page).to have_content("George Lopez")
-      expect(page).to have_content("Age: 43")
+      expect(page).to have_content("Age: 44")
       expect(page).to have_content("Funny")
+    end
+
+    it 'should show average comedian age' do
+      visit '/comedians'
+
+      expect(page).to have_content("Average Age: 50")
     end
   end
 end
