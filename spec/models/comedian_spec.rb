1ad68dc393b4e20e202f0bcdbcd06041ae539a88
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-17 10:44:43 -0600

    adds association tests for comedian and special to specs

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,11 +1,15 @@
 RSpec.describe Comedian do
   describe 'Validations' do
+    it "has many specials" do
+      association = Comedian.reflect_on_association(:specials)
+      expect(association.macro).to eq :has_many
+    end
+
     describe 'Required Field(s)' do
       it 'should be invalid if missing a name' do
         comic = Comedian.create(age: 48)
         expect(comic).to_not be_valid
       end
-
       it 'should be invalid if missing an age' do
         comic = Comedian.create(name: 'Mitch Hedberg')
         expect(comic).to_not be_valid
