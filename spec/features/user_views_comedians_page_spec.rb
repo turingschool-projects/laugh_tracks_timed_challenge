97f590d10ff004c21d19d707d8b1f16a4198ada2
Tim Fielder <fiel1139@gmail.com>
2018-08-17 10:22:07 -0600

    adding to test.

--- spec/features/user_views_comedians_page_spec.rb
+++ spec/features/user_views_comedians_page_spec.rb
@@ -2,6 +2,10 @@ RSpec.describe 'Comedians index page' do
   before(:each) do
     @comedian_1 = Comedian.create(name: 'Ron', age:40)
     @comedian_2 = Comedian.create(name: 'Sally', age:50)
+    @special_1 = Special.create(name: "Rock on")
+    @special_2 = Special.create(name: "Cool")
+    @special_3 = Special.create(name: "Stuff")
+    @special_4 = Special.create(name: "More")
   end
   context 'on opening homepage'
     it 'shows comedians with names and ages' do
@@ -9,6 +13,12 @@ RSpec.describe 'Comedians index page' do
 
       expect(page).to have_content(@comedian_1.name)
       expect(page).to have_content(@comedian_2.name)
+    end
+    it 'shows list of comedians specials' do
+      visit '/comedians'
+
+      expect(page).to have_content(@comedian_1.name)
+      expect(page).to have_content(@comedian_1.name)
     end
 
     # it 'shows song lengths' do
