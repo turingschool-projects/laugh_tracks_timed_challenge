dae5485bb9c7e7b0dea1d62269fd4afef9c15f6e
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-17 10:53:31 -0600

    WIP update index and index spec to draft specials appearing on comedians index page

--- spec/features/comedian_index_spec.rb
+++ spec/features/comedian_index_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe 'Comedian index page' do
   before(:each) do
     iliza = Comedian.create(name: "Iliza Shlesinger", age: 34)
+    Special.create(name: "War Paint", comedian_id: iliza.id)
+    Special.create(name: "Freezing Hot", comedian_id: iliza.id)
+    Special.create(name: "Confirmed Kills", comedian_id: iliza.id)
+
     ali = Comedian.create(name: "Ali Wong", age: 35)
+    Special.create(name: "Baby Cobra", comedian_id: ali.id)
+
   end
 
   it 'should show comedian name' do
@@ -12,6 +18,8 @@ RSpec.describe 'Comedian index page' do
 
   it 'should show comedian age' do
     visit "/comedians"
+
+    save_and_open_page
     expect(page).to have_content('34')
     expect(page).to_not have_content('36')
   end
