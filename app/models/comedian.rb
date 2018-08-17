c44817a8face2e3e4e3e0855287bd7169a6829a5
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-08-17 11:03:11 -0600

    only got user stories 1/3 done, not sure how to relate databases well

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,4 +1,10 @@
 class Comedian < ActiveRecord::Base
   validates :name, presence: true
   validates :age, presence: true
+
+  belongs_to :special
+
+  def self.average_age
+    average(:age)
+  end
 end
--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -6,5 +6,12 @@ RSpec.describe Special do
         expect(special).to_not be_valid
       end
     end
+    describe 'who it belongs to' do
+      it 'belongs to a specific comedian' do
+        association = described_class.reflect_on_association(:comedian)
+        expect(association.macro).to eq :belongs_to
+      end
+    end
   end
+
 end
